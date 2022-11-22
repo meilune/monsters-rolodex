bf9610907d42f101a978f793cb8c1b34cf43831a
Ginja <virginija.meilune@gmail.com>
2022-11-23 05:53:40 +1000

    Updating the states and the way they work

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Virginija'
+      name: {firstName: 'Virginija', lastName: 'Meilune'},
+      company: "Rowland"
     }
   }
 
@@ -18,12 +19,28 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi, {this.state.name}.
+            Hi, I am {this.state.name.firstName} {this.state.name.lastName}. I work at {this.state.company}.
           </p>
-          <button onClick={() => {
-            this.setState({ name: 'Sigita' })
+          <button 
+            onClick={() => {
+              this.setState(
+                () => {
+                  return { 
+                    name: {
+                      firstName: 'Sigita', 
+                      lastName: 'Hunter'
+                    },
+                    company: 'Home'
+                  };
+                },
+                //This function will only run this callback function when all the states have been updated:
+                () => {
+                  console.log(this.state);
+                }
+                );
             }}
-            >Change Name
+          >
+            Change Name
           </button>
         </header>
       </div>
